1eeb1b365dc42306846bab61bda823dbda9fcc68
H-Hiny <1habiba.hisham@gmail.com>
2022-08-13 12:01:29 +0200

    edited main

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define ARRAY_SIZE 10
+
+
+int LongestOcc(int* arr, int key)
+{
+    int i;
+    int maxcount=0;
+    int count=0;
+    for(i=0;i< ARRAY_SIZE; i++)
+    {
+        if(arr[i]==key)
+        {
+            count++;
+        }
+        else
+        {
+            count=0;
+        }
+        if (count>maxcount)
+        {
+            maxcount=count;
+        }
+    }
+    return maxcount;
+
+}
+
+
+
+
+int main()
+{
+    int j;
+    int arr[ARRAY_SIZE];
+    int key;
+    printf("Enter the array: \n");
+    for(j=0; j<ARRAY_SIZE; j++)
+    {
+        scanf("%d",&arr[j]);
+    }
+    printf("Enter key to find the last occurence:\n");
+    scanf("%d",&key );
+    int z=LongestOcc(arr, key);
+    printf("the number of the occurence is %d",z);
+    return 0;
+}
